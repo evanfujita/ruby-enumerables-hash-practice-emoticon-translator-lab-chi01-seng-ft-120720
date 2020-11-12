921efbf75ffedbd3d5eb473ac5414d2957dcf4f7
Evan Fujita <evanfujita@gmail.com>
2020-11-12 23:11:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,48 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(emoticons)
+  emoticons = YAML.load_file('lib/emoticons.yml')
+  hash = emoticons.each_with_object({}) do |(key, value), hash|
+    if !hash[key]
+      hash[key] = {}
+    end
+    if !hash[key][:english]
+      hash[key][:english] = value[0]
+    end
+    if !hash[key][:japanese]
+      hash[key][:japanese] = value[1]
+    end
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticons, emoji)
+    hash = Hash.new
+    load_library(emoticons).each do |key, value|
+        if value[:english].include?("O:)")
+          hash[:japanese] = value[:japanese]
+          #binding.pry
+        elsif 
+           !value[:english].include?(emoji)
+          hash[:japanese] = "Sorry, that emoticon was not found"
+          #binding.pry
+        end
+   end
+   hash[:japanese]
 end
 
-def get_english_meaning
-  # code goes here
-end
+def get_english_meaning(emoticons, emoji)
+  hash = Hash.new
+  load_library(emoticons).each do |key, value|
+        if value[:japanese].include?(emoji)
+          hash[:english] = key
+        else
+          hash[:english] = "Sorry, that emoticon was not found"
+          #binding.pry
+        end
+      end   
+      hash[:english]
+  end
+
+
+
