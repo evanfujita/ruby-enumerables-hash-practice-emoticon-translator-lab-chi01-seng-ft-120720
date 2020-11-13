5100538efc53b3c427fd9445d986ef2aaf1c1f91
Evan Fujita <evanfujita@gmail.com>
2020-11-13 01:25:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(emoticons, emoji)
       load_library(emoticons).find do |key, value| 
         if value[:english] == emoji
           hash = value
-          binding.pry
+          #binding.pry
         elsif false 
           hash[:japanese] = "Sorry, that emoticon was not found"
         end
@@ -33,7 +33,7 @@ def get_english_meaning(emoticons, emoji)
     load_library(emoticons).find do |key, value|
         if value[:japanese] == emoji
           word = key
-          binding.pry
+          #binding.pry
           elsif false
           word = "Sorry, that emoticon was not found"
         end
