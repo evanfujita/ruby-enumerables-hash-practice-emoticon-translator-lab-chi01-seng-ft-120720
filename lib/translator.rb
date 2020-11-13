147ed8dc1c32035531a5956039fde2cee3aef0d8
Evan Fujita <evanfujita@gmail.com>
2020-11-13 00:16:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 def get_japanese_emoticon(emoticons, emoji)
     hash = Hash.new
       load_library(emoticons).find do |key, value| key[:english] == emoji
-        hash << key[:japanese]
+        hash[:japanese] = key[:japanese]
       end
       binding.pry
           puts "YES"
